62ddf043e0b9a4116df5ca304c2e59c48815b418
chiu <chiucode@gmail.com>
2015-06-14 03:19:49 +0000

    added trip model

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,53 @@
+class TripsController < ApplicationController
+
+      def index
+        @trips = Trip.all
+      end
+
+      def show
+        @trip = Trip.find(params[:id])
+      end
+
+      def new
+        @trip = Trip.new
+      end
+
+      def edit
+        @trip = Trip.find(params[:id])
+      end
+
+      def create
+        @trip = Trip.new(trip_params)
+
+        if @trip.save
+          redirect_to trips_path
+        else
+          render :new
+        end
+      end
+
+      def update
+        @trip = Trip.find(params[:id])
+
+        if @trip.update_attributes(trip_params)
+          redirect_to trip_path(@trip)
+        else
+          render :edit
+        end
+      end
+
+      def destroy
+        @trip = Trip.find(params[:id])
+        @trip.destroy
+        redirect_to trips_path
+      end
+
+      protected
+
+      def trip_params
+        params.require(:trip).permit(
+          :title, :start_time, :end_time, :description, :trip_image_url
+        )
+      end
+
+    end
