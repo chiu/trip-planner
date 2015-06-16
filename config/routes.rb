80702b0b16c23e58bfeb317f24d8d5bf53483484
Ramtin Radfar <ramtinr@gmail.com>
2015-06-15 22:37:51 -0700

    remove devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,10 @@
 Rails.application.routes.draw do
 
-
-
-  devise_for :users
-
-
   resources :trips do
    resources :waypoints, only: [:new, :create]
- end 
-
-
- # root to: 'front_page#front_page'
+  end 
 
   root to: 'trips#front_page'
-
 
   # get 'trips/show'
 
