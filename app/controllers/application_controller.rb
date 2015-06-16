45cb1e0d590f5fab450965062af4de0f3fe38462
Ramtin Radfar <ramtinr@gmail.com>
2015-06-16 15:03:40 -0700

    implement user authentication and route navbar buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+ 
+  protected
+
+  def restrict_access
+    if !current_user
+      redirect_to new_session_path, notice: 'You must log in.'
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+  end
+
+  helper_method :current_user
 end
