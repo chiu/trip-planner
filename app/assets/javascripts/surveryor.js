0bf7bcff675a6cd0cf889de86faf20c79aa754d0
chiu <chiucode@gmail.com>
2015-06-17 02:00:05 +0000

    great, cleaned up code, surveyor pseudo class has google map methods

--- app/assets/javascripts/surveryor.js
+++ app/assets/javascripts/surveryor.js
@@ -15,90 +15,88 @@
 
 // s.drawDirections();
 
-var surveyor = (function()
-{
-  "use strict";
-  var rendererOptions = {
-      draggable: true
-  };
-  var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
-  var directionsService = new google.maps.DirectionsService();
-  var map;
+var surveyor = (function() {
+    "use strict";
+    var rendererOptions = {
+        draggable: true
+    };
+    var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+    var directionsService = new google.maps.DirectionsService();
+    var map;
 
-  var australia = new google.maps.LatLng(-25.274398, 133.775136);
+    var australia = new google.maps.LatLng(-25.274398, 133.775136);
 
-  function initialize() {
-
-
-      var mapOptions = {
-          zoom: 7,
-          center: australia
-      };
-      map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-      directionsDisplay.setMap(map);
-      directionsDisplay.setPanel(document.getElementById('directionsPanel'));
-      google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
-          computeTotalDistance(directionsDisplay.getDirections());
-          showWaypoints(directionsDisplay.getDirections());
-      });
-  }
+    function initialize() {
 
 
-  function drawDirections() {
-      console.log("draw directions happened");
-      var startlocation = $("#origin_field").val();
-      console.log("start:", startlocation);
-      var endlocation = $("#dest_field").val();
-      console.log("end:", endlocation);
-      calcRoute(startlocation, endlocation);
-  }
+        var mapOptions = {
+            zoom: 7,
+            center: australia
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        directionsDisplay.setMap(map);
+        directionsDisplay.setPanel(document.getElementById('directionsPanel'));
+        google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
+            computeTotalDistance(directionsDisplay.getDirections());
+            showWaypoints(directionsDisplay.getDirections());
+        });
+    }
 
-  function calcRoute(origin_argument, destination_argument) {
-      var request = {
-          origin: origin_argument,
-          destination: destination_argument,
-          waypoints: [{
-              location: 'Vancouver, BC'
-          }, {
-              location: 'Richmond, BC'
-          }],
-          travelMode: google.maps.TravelMode.DRIVING
-      };
-      directionsService.route(request, function(response, status) {
-          if (status == google.maps.DirectionsStatus.OK) {
-              directionsDisplay.setDirections(response);
-          }
-      });
-  }
 
-  function showWaypoints(result) {
-      var myroute = result.routes[0];
-      for (var i = 0; i < myroute.legs.length; i++) {
-          console.log(myroute.legs[i]);
-      }
-  }
+    function drawDirections() {
+        console.log("draw directions happened");
+        var startlocation = $("#origin_field").val();
+        console.log("start:", startlocation);
+        var endlocation = $("#dest_field").val();
+        console.log("end:", endlocation);
+        calcRoute(startlocation, endlocation);
+    }
 
-  function computeTotalDistance(result) {
-      var total = 0;
-      var myroute = result.routes[0];
-      for (var i = 0; i < myroute.legs.length; i++) {
-          total += myroute.legs[i].distance.value;
-      }
-      total = total / 1000.0;
-      document.getElementById('total').innerHTML = total + ' km';
-  }
+    function calcRoute(origin_argument, destination_argument) {
+        var request = {
+            origin: origin_argument,
+            destination: destination_argument,
+            waypoints: [{
+                location: 'Vancouver, BC'
+            }, {
+                location: 'Richmond, BC'
+            }],
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionsService.route(request, function(response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            }
+        });
+    }
 
-  return {
-      showWaypoints: showWaypoints,
-      drawDirections: drawDirections,
-      initialize: initialize
-  };
+    function showWaypoints(result) {
+        var myroute = result.routes[0];
+        for (var i = 0; i < myroute.legs.length; i++) {
+            console.log(myroute.legs[i]);
+        }
+    }
+
+    function computeTotalDistance(result) {
+        var total = 0;
+        var myroute = result.routes[0];
+        for (var i = 0; i < myroute.legs.length; i++) {
+            total += myroute.legs[i].distance.value;
+        }
+        total = total / 1000.0;
+        document.getElementById('total').innerHTML = total + ' km';
+    }
+
+    return {
+        showWaypoints: showWaypoints,
+        drawDirections: drawDirections,
+        initialize: initialize
+    };
 }());
 
 $(document).on("click", "#calculateRouteButton", surveyor.drawDirections);
 
-$(function()
-{
-  "use strict";
-  google.maps.event.addDomListener(window, 'load', surveyor.initialize);
+$(function() {
+    "use strict";
+    google.maps.event.addDomListener(window, 'load', surveyor.initialize);
 });
