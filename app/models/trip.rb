06dd948ecab5cea36c0767ada898e906e6067bbf
chiu <chiucode@gmail.com>
2015-06-23 23:07:48 +0000

    changed trip user model associations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,8 +1,11 @@
 class Trip < ActiveRecord::Base
   # attr_accessible :waypoints_attributes
   has_many :waypoints
-  belongs_to :user
-  has_many :users
+  # belongs_to :user
+  has_many :journeys
+  has_many :users, through: :journeys
+
+
   has_one :new_waypoint, class_name: 'Waypoint'
   accepts_nested_attributes_for :new_waypoint
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ActiveRecord::Base
   has_secure_password
   
   has_many :journeys
-  has_many :trips
+  has_many :trips, through: :journeys
+  
 end
+
+
+
+
