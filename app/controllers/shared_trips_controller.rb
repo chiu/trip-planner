4a011523c9028ec0e2703244e4809c95ac5b9cfc
chiu <chiucode@gmail.com>
2015-06-25 05:54:13 +0000

    now working params has to be unique?

--- app/controllers/shared_trips_controller.rb
+++ app/controllers/shared_trips_controller.rb
@@ -26,9 +26,11 @@ class SharedTripsController < ApplicationController
     @shared_trip = SharedTrip.new(shared_trip_params)
     @shared_trip.shared_id = params[:user_id]
     @shared_trip.trip_id = params[:trip_id]
+    # @shared_trip.user_id = params[:user_id]
+
 
     if @shared_trip.save
-      redirect_to(:back)
+      redirect_to trip_shared_trips_path
     else
       render :new
     end
