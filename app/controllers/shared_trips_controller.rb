a35225c93643095e8341410355d1f8abdf0134e0
chiu <chiucode@gmail.com>
2015-06-25 06:35:59 +0000

    can now print guest list

--- app/controllers/shared_trips_controller.rb
+++ app/controllers/shared_trips_controller.rb
@@ -5,48 +5,61 @@ class SharedTripsController < ApplicationController
 
 
   def index
-    @shared_trips = SharedTrip.all
+    @shared_trips = SharedTrip.where(trip_id: params[:trip_id])
     @trips = Trip.all
     @users = User.all
-    render layout: false
-
-  end
-
-
-  def new
-
-    @users = User.all
-    # @trip = Trip.find(params[:trip_id])
-    @shared_trip = SharedTrip.new
-    render layout: false
-
-  end
+# @users = User.all
+# @users = SharedTrip.users
+# Client.where(first_name: 'Lifo')
 
 
-  def create
-    # @shared_trip = SharedTrip.new(shared_trip_params)
-    # @shared_trip.user_id = params[:user_id]
-    # @shared_trip.trip_id = params[:trip_id]
-    # @shared_trip.user_id = params[:user_id]
+# @user_array = []
+
+
+# @shared_trips.each do |shared_trip| 
+#   @user_array.push(shared_trip.)
+# end
+
+
+render layout: false
+
+end
+
+
+def new
+
+  @users = User.all
+# @trip = Trip.find(params[:trip_id])
+@shared_trip = SharedTrip.new
+render layout: false
+
+end
+
+
+def create
+# @shared_trip = SharedTrip.new(shared_trip_params)
+# @shared_trip.user_id = params[:user_id]
+# @shared_trip.trip_id = params[:trip_id]
+# @shared_trip.user_id = params[:user_id]
 
 @shared_trip = @trip.shared_trips.build(shared_trip_params)
 
-    if @shared_trip.save
-      redirect_to trip_shared_trips_path
-    else
-      render :new
-    end
-  end
+if @shared_trip.save
+  redirect_to trip_shared_trips_path
+else
+  render :new
+end
+end
 
-  protected
-  def load_trip
-    @trip = Trip.find(params[:trip_id])
+protected
+def load_trip
+  @trip = Trip.find(params[:trip_id])
 
-  end
-  def shared_trip_params
-    params.require(:shared_trip).permit(
-      :shared_id, :user_id, :trip_id
-      )
-  end
+end
+def shared_trip_params
+  params.require(:shared_trip).permit(
+    :shared_id, :user_id, :trip_id
+    )
+end
 
 end
